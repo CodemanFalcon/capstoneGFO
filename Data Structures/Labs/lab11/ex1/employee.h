2880e51c5e85254bf7faa76cfeb1e60a9d13c897
ju1ez <47983571+ju1ez@users.noreply.github.com>
2019-02-25 11:47:10 -0500

    Add files via upload

---
+++ Data Structures/Labs/lab11/ex1/employee.h
@@ -0,0 +1,26 @@
+//This is the HEADER FILE employee.h. 
+//This is the INTERFACE for the class Employee. 
+//This is primarily intended to be used as a base class to derive 
+//classes for different kinds of employees. 
+#ifndef EMPLOYEE_H 
+#define EMPLOYEE_H
+#include <string> 
+using namespace std;
+namespace kentemployees {
+  class Employee {
+    public:
+      Employee();
+    Employee(string new_name, string new_ssn);
+    string get_name();
+    string get_ssn();
+    void change_name(string new_name);
+    void change_ssn(string new_ssn);
+    void print_check();
+    void give_raise(double amount);
+    protected:
+      string name;
+    string ssn;
+    double net_pay;
+  };
+}
+#endif //EMPLOYEE_H
