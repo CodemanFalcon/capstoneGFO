d198ac0779a5091b70c9346c6e3984bacc3f19ef
ju1ez <47983571+ju1ez@users.noreply.github.com>
2019-02-25 11:49:39 -0500

    Add files via upload

---
+++ Data Structures/Labs/lab11/ex3/discountsale.cpp
@@ -0,0 +1,17 @@
+//This is the IMPLEMENTATION for the class DiscountSale. 
+#include "discountsale.h"
+
+namespace kentB 
+{ 
+    DiscountSale::DiscountSale() : Sale(), discount(0) 
+    {}
+
+    DiscountSale::DiscountSale(double the_price, double the_discount) : Sale(the_price), discount(the_discount) 
+    {}
+
+    double DiscountSale::bill() const 
+    { 
+        double fraction = discount/100; 
+        return (1 - fraction)*price; 
+    } 
+}//kentB
