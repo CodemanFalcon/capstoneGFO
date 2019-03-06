9549273094a5a2dfe2b723dad4c4ca0266275d3c
Jules <47983571+ju1ez@users.noreply.github.com>
2019-03-06 16:48:20 -0500

    Delete footer.php

--- Capstone/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="status_msg">
-        <p style="float: left;"><i>&copy; May 2019 INSERT NAMES</i> | CS 44901 Capstone Project</p>
-        	<div class="elementToFadeInAndOut">             
-              <?php echo $message; ?>
-         </div>
- </div>
