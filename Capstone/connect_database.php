f513f149c46fa44d12e4b9cf473ff3244396ac03
Jules <47983571+ju1ez@users.noreply.github.com>
2019-03-06 16:48:27 -0500

    Delete connect_database.php

--- Capstone/connect_database.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$conn = mysqli_connect("localhost","jules", "capstone","jules");
-	if (!$conn){die ($message = "<span style='color: #ff8080;'>Failed to connect to database: </span>" . mysqli_connect_error());}
-	else { $message = "<span style='color: #59a7ab;'>Database connection has been established successfully</span>";}
-
-	//echo $message; //test database connection
-?>
